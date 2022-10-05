95b7d6c5e5c3af2a850a94f3dd3894637b245750
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-05 15:08:10 +0900

    feat: correction menu-bar, create vedio-contents
    메뉴바들 색상, 사이즈 등 수정
    비디오 썸네일 몇 가지 추가

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@
 
 const thisYear = document.querySelector(".this-year");
 thisYear.textContent = new Date().getFullYear();
+
+
+// 계속 마우스 오버하여 재생하기
+
+// const thumbnailEl = document.getElementsByClassName(".vedio__thumbnail");
+// const timeEl = document.getElementsByClassName(".vedio__time");
+
+// function mouseOver () {
+//   if (timeEl.innerText === null) {
+//     timeEl.innerText = "계속 마우스 오버하여 재생하기"
+//   } else {
+//     timeEl.innerText = ""
+//     timeEl.innerText = "계속 마우스 오버하여 재생하기"
+//   }
+// }
+
+// thumbnailEl.addEventListener("mouseover", mouseOver);
