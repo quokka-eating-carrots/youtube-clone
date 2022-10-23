f603890e0a46d62b3b56233b11c1604423b4bd91
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-23 21:34:41 +0900

    Feat: Fix Feedback

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ thisYear.textContent = new Date().getFullYear();
 
 // 계속 마우스 오버하여 재생하기
 
-const thumbnailEl = document.getElementsByClassName(".video__thumbnail");
-const timeEl = document.getElementsByClassName(".video__time");
+const thumbnailEl = document.getElementsByClassName("video__thumbnail");
+const timeEl = document.getElementsByClassName("video__time");
 
-function mouseOver () {
-  if (timeEl.innerText === null) {
-    timeEl.innerText = "계속 마우스 오버하여 재생하기"
-  } else {
-    timeEl.innerText = ""
-    timeEl.innerText = "계속 마우스 오버하여 재생하기"
-  }
+
+for (let i = 0; i < thumbnailEl.length; i += 1) {
+  const originalTime = timeEl[i].innerText;
+  thumbnailEl[i].addEventListener("mouseover", function () {
+    timeEl[i].innerText = "계속 마우스 오버하여 재생하기";
+  })
+  thumbnailEl[i].addEventListener("mouseout", function () {
+    timeEl[i].innerText = originalTime;
+  })
 }
-
-thumbnailEl.addEventListener("mouseover", mouseOver);
